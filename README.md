38e365eb48bc690f080b7b765c758add2760aada
jsegal <jsegal@iastate.edu>
2020-09-02 00:40:12 -0500

    🎉 Scaffold Web Page

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Web-DNA
+
 Diversity, Netiquette, Accessibility and Inclusion in Language and Terminology; Standards
---
+++ pages/index.js
@@ -0,0 +1,114 @@
+import Head from 'next/head'
+import Content from './main.mdx'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Web DNA</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1 className="title">
+          Welcome to <a href="https://github.com/DSC-ISU/web-dna">Web DNA</a>
+        </h1>
+        <div className="content">
+          <Content />
+        </div>
+      </main>
+
+      <footer>
+        <a
+          href="https://dsc.community.dev/iowa-state-university/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Built with ♥ by DSC@ISU
+        </a>
+      </footer>
+
+      <style jsx>{`
+        .container {
+          min-height: 100vh;
+          padding: 0 0.5rem;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        main {
+          padding: 3rem 0rem;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        .content {
+          padding: 1rem 5rem;
+          max-width: 800px;
+        }
+
+        footer {
+          width: 100%;
+          height: 100px;
+          border-top: 1px solid #eaeaea;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
+        footer a {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
+        a {
+          color: inherit;
+          text-decoration: none;
+        }
+
+        .title a {
+          color: #0070f3;
+          text-decoration: none;
+        }
+
+        .title a:hover,
+        .title a:focus,
+        .title a:active {
+          text-decoration: underline;
+        }
+
+        .title {
+          margin: 0;
+          line-height: 1.15;
+          font-size: 4rem;
+        }
+
+        .title,
+        .description {
+          text-align: center;
+        }
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  )
+}
